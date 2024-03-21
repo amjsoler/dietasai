50a6945c198206c2b6731c3669bbd797524abe21
Jorge Amat Soler <amjsoler@gmail.com>
2024-03-21 14:49:38 +0100

    csrf

--- resources/views/prompt.blade.php
+++ resources/views/prompt.blade.php
@@ -1,4 +1,5 @@
 <form action="{{route("postprompt")}}" method="POST">
+    {{csrf_field()}}
     <fieldset>
         <textarea autofocus style="width: 100%; height: 400px;" name="prompt"></textarea>
     </fieldset>
