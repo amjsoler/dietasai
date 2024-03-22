dc379238a9608fac578c39f4f590d6c4593fc65f
Jorge Amat Soler <amjsoler@gmail.com>
2024-03-23 00:33:15 +0100

    Se ha quitado la vista de prompt puesto que se ha eliminado ya esa ruta

--- resources/views/prompt-recipe-list.blade.php
+++ resources/views/prompt-recipe-list.blade.php
@@ -1,39 +1,37 @@
-@isset($result)
+@if(isset($result))
     <p>{{$result}}</p>
-@endisset
+@else
 
-@isset($recetas)
-    Recetas generadas:
-    <ul>
-    @foreach($recetas as $receta)
-        <li>{{$receta}}</li>
-    @endforeach
-    </ul>
-@endisset
+    <div style="max-width:500px; margin:0 auto;">
+        <h1>Creación de un listado de recetas provisionales</h1>
+        <p>Este formulario permite crear un listado de nombres de recetas provisionales a partir de un contexto y un prompt.</p>
+        <p>Ten en cuenta que puedes dejar los campos vacíos, en cuyo caso, se utilizará el contexto y prompt por defecto.</p>
+        <p>Si on especificas el número de recetas, se generará una única receta</p>
 
-@isset($recetasAInsertar)
-    Recetas a insertar:
-    <ul>
-        @foreach($recetasAInsertar as $receta)
-            <li>{{$receta}}</li>
-        @endforeach
-    </ul>
-@endisset
+        <hr>
 
-<form action="{{route("postpromptrecipelist")}}" method="POST">
-    {{csrf_field()}}
-    <fieldset>
-        <textarea autofocus style="width: 100%; height: 200px;" name="context"></textarea>
-    </fieldset>
+        <form style="" action="{{route("postpromptrecipelist")}}" method="POST">
+            {{csrf_field()}}
+            <label style="display: block; margin-top: 25px;">
+                <span>Contexto</span>
+                <textarea autofocus style="width: 100%; height: 200px;" name="context"></textarea>
+            </label>
 
-    <fieldset>
-        <textarea autofocus style="width: 100%; height: 200px;" name="prompt"></textarea>
-    </fieldset>
+            <label style="display: block; margin-top: 25px;">
+                <span>Prompt</span>
+                <textarea autofocus style="width: 100%; height: 200px;" name="prompt"></textarea>
+            </label>
 
-    <fieldset>
-        <input type="number" name="num_recipes" placeholder="Enter number of recipes">
-    </fieldset>
-    <fieldset>
-        <input type="submit" value="Submit">
-    </fieldset>
-</form>
+            <div style="display: flex; flex-direction: row; align-items: center; justify-content: space-between">
+                <label style="display: flex; flex-direction: column; margin-top: 25px;">
+                    <span>Número de recetas a generar</span>
+                    <input type="number" name="num_recipes">
+                </label>
+
+                <label style="display: block; margin-top: 25px;">
+                    <input type="submit" value="Encolar creación de recetas">
+                </label>
+            </div>
+        </form>
+    </div>
+@endif
